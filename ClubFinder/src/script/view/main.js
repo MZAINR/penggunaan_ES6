ceb51ae9a33ed0aefd58f2e0be9f0fe34bec9e55
MZAINR <MZAINR>
2023-09-13 17:29:29 +0700

    menerapkan destructuring object pada rederResult dan menghapus beberapa comment

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -11,18 +11,11 @@ const main = function () {
   const renderResult = function (results) {
     clubListElement.innerHTML = '';
     results.forEach(function (club) {
-      const name = club.name;
-      const fanArt = club.fanArt;
-      const description = club.description;
+      const {name, fanArt, description} = club;
 
       const clubElement = document.createElement('div');
       clubElement.setAttribute('class', 'club');
 
-      // clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-      //     '<div class="club-info">\n' +
-      //     '<h2>' + name + '</h2>\n' +
-      //     '<p>' + description + '</p>' +
-      //     '</div>';
       clubElement.innerHTML = 
         `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
         <div class="club-info">
@@ -37,7 +30,6 @@ const main = function () {
 
   const fallbackResult = function (message) {
     clubListElement.innerHTML = '';
-    // clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>';
     clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   };
 
