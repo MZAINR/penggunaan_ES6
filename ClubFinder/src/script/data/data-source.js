16156a5755fcca57b07883bcf2551172d5f36e8b
MZAINR <MZAINR>
2023-09-15 14:46:43 +0700

    mengubah method keyword menjadi method yang sesuai standar ES6

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -3,18 +3,31 @@ class DataSource {
     this.onSuccess = onSuccess;
     this.onFailed = onFailed;
   }
+
+
+  searchClub(keyword) {
+    const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+  
+    if (filteredClubs) {
+      this.onSuccess(filteredClubs);
+    } else {
+      this.onFailed(`${keyword} is not found`)
+    }
+  }
 }
 
-DataSource.prototype.searchClub = function (keyword) {
-  const filteredClubs = clubs.filter((club) => {
-    return club.name.toUpperCase().includes(keyword.toUpperCase());
-  });
+// DataSource.prototype.searchClub = function (keyword) {
+//   const filteredClubs = clubs.filter((club) => {
+//     return club.name.toUpperCase().includes(keyword.toUpperCase());
+//   });
 
-  if (filteredClubs.length) {
-    this.onSuccess(filteredClubs);
-  } else {
-    this.onFailed(`${keyword} is not found`);
-  }
-};
+//   if (filteredClubs.length) {
+//     this.onSuccess(filteredClubs);
+//   } else {
+//     this.onFailed(`${keyword} is not found`);
+//   }
+// };
+
+
 
 //test comment
