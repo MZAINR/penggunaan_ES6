ff34e4db0ceb6dd74fd74d154edc709bc6e98646
MZAINR <MZAINR>
2023-09-19 11:22:42 +0700

    menerapkan fungsi render agar isi dari elemen bisa diubah

--- custom-element/image-figure.js
+++ custom-element/image-figure.js
@@ -4,13 +4,25 @@ class ImageFigure extends HTMLElement {
     this.alt = this.getAttribute('alt') || null;
     this.width = this.getAttribute('width') || null
     this.caption = this.getAttribute('caption') || null;
+    this.render();
+  }
 
-    this.innerHTML = `
-      <figure>
-        <img src='${this.src}' alt='${this.alt}' width='${this.width}'>
-        <figcaption>${this.caption}</figcaption>
-      </figure>
-    `
+render() {
+  this.innerHTML = `
+  <figure>
+    <img src='${this.src}' alt='${this.alt}' width='${this.width}'>
+    <figcaption>${this.caption}</figcaption>
+  </figure>
+`;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this[name] = newValue;
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return['caption'];
   }
 }
 
